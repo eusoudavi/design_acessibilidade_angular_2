e597ca4e701c65617278e906c3c69dea071db4d8
davi.visintainer <davi.visintainer@placeti.com.br>
2023-01-24 18:34:02 -0300

    fechando o modal clicando fora dele

--- src/app/shared/component/modal/modal.component.ts
+++ src/app/shared/component/modal/modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, HostBinding, OnInit, TemplateRef} from '@angular/core';
 import {ModalConfig} from './interfaces/modal-config';
-import {fade} from '../../../animations/fade';
+import {fade} from '../../animations/fade';
+import {ModalRef} from './models/model-ref';
 
 @Component({
   selector: 'app-modal',
@@ -11,6 +12,7 @@ import {fade} from '../../../animations/fade';
 export class ModalComponent implements OnInit {
   @HostBinding('@fade') fade = true;  // AQUI ESTAMOS PASSANDO O FADE PARA TODO O HOST DO MODAL
   public config: ModalConfig;
+  public modalRef: ModalRef;
 
   constructor() { }
 
