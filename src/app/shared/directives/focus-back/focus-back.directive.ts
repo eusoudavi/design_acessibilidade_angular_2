4abba2103b15750811ab9d19de9e0449db771db4
davi.visintainer <davi.visintainer@placeti.com.br>
2023-02-08 16:37:34 -0300

    retornando o foco

--- src/app/shared/directives/focus-back/focus-back.directive.ts
+++ src/app/shared/directives/focus-back/focus-back.directive.ts
@@ -1,6 +1,19 @@
-import {Directive} from '@angular/core';
+import {Directive, OnDestroy, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appFocusBack]'
 })
-export class FocusBackDirective {}
+export class FocusBackDirective implements OnInit, OnDestroy{
+
+  private lastFocusedElement: Element;
+
+  public ngOnInit(): void {
+    this.lastFocusedElement = document.activeElement;
+  }
+
+  public ngOnDestroy(): void {
+    if (this.lastFocusedElement) {
+      (this.lastFocusedElement as HTMLElement).focus();
+    }
+  }
+}
