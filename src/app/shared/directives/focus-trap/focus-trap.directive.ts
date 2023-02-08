4b3fbf2ab7d9199fe2d67798931d2557711a0a49
davi.visintainer <davi.visintainer@placeti.com.br>
2023-02-08 15:36:32 -0300

    colocando o foco no primeiro elemento

--- src/app/shared/directives/focus-trap/focus-trap.directive.ts
+++ src/app/shared/directives/focus-trap/focus-trap.directive.ts
@@ -1,9 +1,27 @@
-import {Directive} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appFocusTrap]'
 })
-export class FocusTrapDirective {
+export class FocusTrapDirective implements AfterViewInit{
 
+  private firstFocusableElement: HTMLElement = null;
+  private lastFocusabelElement: HTMLElement = null;
+
+  constructor(private elementRef: ElementRef<any>) { }
+
+  ngAfterViewInit(): void {
+    const focusableElements = this.elementRef.nativeElement.querySelectorAll(`
+        [tabindex]:not([tabindex="-1"]),
+        a[href]:not([disabled]),
+        button:not([disabled]),
+        textarea:not([disabled]),
+        input:not([disabled]),
+        select:not([disabled])`);
+
+    this.firstFocusableElement = focusableElements[0];
+    this.lastFocusabelElement = focusableElements[focusableElements - 1];
+    this.firstFocusableElement.focus();
+  }
 
 }
