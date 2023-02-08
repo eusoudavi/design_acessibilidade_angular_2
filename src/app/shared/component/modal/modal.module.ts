c330174ea0985be219675f3af5c6efb06ab5a42a
davi.visintainer <davi.visintainer@placeti.com.br>
2023-02-08 16:30:24 -0300

    inicio do retorno do foco

--- src/app/shared/component/modal/modal.module.ts
+++ src/app/shared/component/modal/modal.module.ts
@@ -3,13 +3,14 @@ import {CommonModule} from '@angular/common';
 import {ModalComponent} from './modal.component';
 import {ModalService} from './services/modal.service';
 import {FocusTrapModule} from '../../directives/focus-trap/focus-trap.module';
-
+import {FocusBackModule} from '../../directives/focus-back/focus-back.module';
 
 @NgModule({
   declarations: [ ModalComponent ],
   imports: [
     CommonModule,
-    FocusTrapModule
+    FocusTrapModule,
+    FocusBackModule
   ],
   exports: [ ModalComponent ],
   providers: [ ModalService ]
